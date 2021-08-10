43bb135d48d655bd7cf528bc3a6bc24ab138bcf3
Felipe Slaughter-Quintero <fslauq@gmail.com>
2021-08-10 19:01:18 -0400

    completed code for index.js, server.js users-routes and middleware. Testing API endpoints next.

--- api/server.js
+++ api/server.js
@@ -6,6 +6,11 @@ const server = express();
 server.use(express.json());
 
 // global middlewares and the user's router need to be connected here
+const usersRouter = require('./users/users-router.js');
+const { logger, validateUserId, validateUser, validatePost } = require('./middleware/middleware.js');
+
+
+server.use('/api/users', usersRouter);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,44 +1,104 @@
 const express = require('express');
 
 // You will need `users-model.js` and `posts-model.js` both
+const Users = require('./users-model.js');
+const Posts = require('../posts/posts-model.js');
+
 // The middleware functions also need to be required
+const { logger, validateUserId, validateUser, validatePost } = require('../../middleware/middleware.js');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/', logger, (req, res) => {
   // RETURN AN ARRAY WITH ALL THE USERS
-});
+  Users.find(req.query)
+    .then((users) => {
+      res.status(200).json(users);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({message: 'An error occurred'});
+    })
+  });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', validateUserId, (req, res) => {
   // RETURN THE USER OBJECT
   // this needs a middleware to verify user id
+  res.status(200).json(req.user);
 });
 
-router.post('/', (req, res) => {
+router.post('/', validateUser, (req, res) => {
   // RETURN THE NEWLY CREATED USER OBJECT
   // this needs a middleware to check that the request body is valid
+  Users.insert(req.body)
+    .then((user) => {
+      res.status(201).json(user);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({message: 'Could not create new user'});
+    }
+  );
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', validateUserId, validateUser, (req, res) => {
   // RETURN THE FRESHLY UPDATED USER OBJECT
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
+  Users.update(req.params.id, req.body)
+    .then((user) => {
+      res.status(201).json(user);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({message: 'The user could not be updated'});
+    }
+  );
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', validateUserId, (req, res) => {
   // RETURN THE FRESHLY DELETED USER OBJECT
   // this needs a middleware to verify user id
+  Users.remove(req.params.id)
+    .then((user) => {
+      res.status(200).json(user);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({message: 'The user could not be deleted'});
+    }
+  );
 });
 
-router.get('/:id/posts', (req, res) => {
+router.get('/:id/posts', validateUserId, (req, res) => {
   // RETURN THE ARRAY OF USER POSTS
   // this needs a middleware to verify user id
+  Posts.getById(req.params.id)
+    .then((posts) => {
+      res.status(200).json (posts);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({message: 'There was an error retrieving the users posts'});
+    }
+  );
 });
 
-router.post('/:id/posts', (req, res) => {
+router.post('/:id/posts', validateUserId, validatePost, (req, res) => {
   // RETURN THE NEWLY CREATED USER POST
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
+  const postInfo = {...req.post, userId: req.user.id};
+  Posts.insert(postInfo)
+    .then((post) => {
+      res.status(210).json(post);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({message: 'There was an error creating the new post'});
+    }
+  );
 });
 
 // do not forget to export the router
+module.exports = router;
